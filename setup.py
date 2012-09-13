d2fccfb353bcce0c509c3bd41712e451dd47a2d1
Mikhail Sobolev <mss@mawhrin.net>
2012-09-13 14:50:41 +0300

    cosmetic changes to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 import os
+
 from setuptools import setup
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "python-derek",
-    version = "0.0.1",
-    author = "Dmitry Rozhkov",
-    author_email = "dmitry.rojkov@gmail.com",
-    description = ("Python client for Derek"),
-    license = "GPL",
-    keywords = "derek",
+    name="python-derek",
+    version="0.0.1",
+    author="Dmitry Rozhkov",
+    author_email="dmitry.rojkov@gmail.com",
+    description="Python client for Derek",
+    license="GPL",
+    keywords="derek",
     packages=['derek'],
     long_description=read('README.rst'),
     classifiers=[
