5c17a391c871db4fcd1f12f39f975be62914a396
Mikhail Sobolev <mss@mawhrin.net>
2012-10-23 15:50:50 +0300

    add dput like options

--- derek/misc.py
+++ derek/misc.py
@@ -7,10 +7,22 @@ from derek.cli_registry import Argument, command
 # To be reviewed
 
 @command([
+    Argument('-D', action='store_true',
+             help='run dinstall after upload'),
+    Argument('-e', '--delayed', metavar='DELAYED', default=None,
+             help="Upload to a delayed queue. Takes an argument from 0 to 15"),
+    Argument('-f', '--force', action='store_true', help="Force an upload"),
+    Argument('-l', action='store_true', help="Run lintian before upload"),
+    Argument('-U', action='store_true',
+             help="Do not write a .upload file after uploading"),
+    Argument('-s', action='store_true', help="Simulate the upload only"),
+    Argument('-u', action='store_true', help="Don't check GnuPG signature"),
+    Argument('-V', action='store_true',
+             help="Check the package version and then upload it"),
     Argument('-o', '--check-only', action='store_true',
                 help='check package(s) only and do not upload')
 ])
-def upload(env):
+def upload(_env):
     "upload new package(s)"
 
 @command([
