1455bbab5e87270cc82e96fa6e9ae5aa0ba9eca8
Mikhail Sobolev <mss@mawhrin.net>
2012-11-01 11:59:34 +0200

    cosmetic changes

--- setup.py
+++ setup.py
@@ -3,11 +3,26 @@ import os
 from setuptools import setup
 
 def read(fname):
+    """
+    read the content of the specified file
+    """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def version():
+    """
+    determine version based on debian/changelog
+    """
+    with open('debian/changelog', 'r') as changelog:
+        first_line = changelog.readline()
+
+        _, rest = first_line.split('(', 1)
+        result, _ = rest.split(')', 1)
+
+    return result
 
 setup(
     name="python-derek",
-    version="0.0.1",
+    version=version(),
     author="Dmitry Rozhkov",
     author_email="dmitry.rojkov@gmail.com",
     description="Python client for Derek",
